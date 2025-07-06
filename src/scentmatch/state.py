eb8884b463e2f13a4a5b6f374e74dd4bbd470e66
kvpratama <kvpratama@gmail.com>
2025-07-06 17:50:18 +0900

    Update state.py

--- src/scentmatch/state.py
+++ src/scentmatch/state.py
@@ -1,15 +1,15 @@
 from typing import List, Dict
 from langgraph.graph import MessagesState
 
-# Define the state type with annotations
+
 class WorkflowState(MessagesState):
-    system_message: str
     about_user: List[Dict[str, str]]
-    final_answer: str
+    sales_pitch: str
+
 
 class WorkflowStateInput(MessagesState):
-    system_message: str
     about_user: List[Dict[str, str]]
 
+
 class WorkflowStateOutput(MessagesState):
-    final_answer: str
+    sales_pitch: str
