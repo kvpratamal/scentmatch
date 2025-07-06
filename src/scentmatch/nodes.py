75f7fa6298d39738f6f568c4103a08298a95a957
kvpratama <kvpratama@gmail.com>
2025-07-06 17:50:06 +0900

    Update nodes.py

--- src/scentmatch/nodes.py
+++ src/scentmatch/nodes.py
@@ -1,5 +1,33 @@
 from scentmatch.state import WorkflowState
 from scentmatch.configuration import Configuration
+import random
+from langchain.chat_models import init_chat_model
+import os
+
 
 def sales_node(state: WorkflowState, config: Configuration):
-    return {}
+    user_response = "\n".join(
+        f"{k}: {v}" for about_user in state["about_user"] for k, v in about_user.items()
+    )
+
+    # Get the available products from the config
+    available_products = config["configurable"]["available_products"]
+    chosen_product = random.choice(available_products)
+    product_description = os.path.join(
+        "src/scentmatch/products", chosen_product + ".txt"
+    )
+    with open(product_description, "r") as f:
+        product_description = f.read()
+
+    # Get the prompt from the file
+    prompt = os.path.join("src/scentmatch/prompts", "sales_prompt.txt")
+    with open(prompt, "r") as f:
+        prompt = f.read()
+    sales_prompt = prompt.format(
+        user_response=user_response, product_description=product_description
+    )
+
+    llm = init_chat_model(config["configurable"]["model"], temperature=1)
+    sales_pitch = llm.invoke(sales_prompt)
+
+    return {"sales_pitch": sales_pitch}
