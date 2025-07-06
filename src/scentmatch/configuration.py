623bdf211fbf07dcc3523ccdcb051f081468fa32
kvpratama <kvpratama@gmail.com>
2025-07-06 17:49:55 +0900

    Update configuration.py

--- src/scentmatch/configuration.py
+++ src/scentmatch/configuration.py
@@ -4,9 +4,10 @@ from typing import Annotated, Literal
 from pydantic import BaseModel, Field
 import os
 
-available_prods= [
+available_prods = [
     file[:-4] for file in os.listdir("src/scentmatch/products") if file.endswith(".txt")
 ]
+
 
 class Configuration(BaseModel):
     """The configuration for the agent."""
@@ -14,23 +15,23 @@ class Configuration(BaseModel):
     system_prompt: str = Field(
         default="You are a helpful AI assistant.",
         description="The system prompt to use for the agent's interactions. "
-        "This prompt sets the context and behavior for the agent."
+        "This prompt sets the context and behavior for the agent.",
     )
 
     model: Annotated[
-            Literal[
-                "google_genai/gemma-3-12b-it",
-                "google_genai/gemma-3-27b-it",
-                "google_genai/gemini-2.5-flash-lite-preview-06-17"
-            ],
-            {"__template_metadata__": {"kind": "llm"}},
-        ] = Field(
-            default="google_genai/gemma-3-12b-it",
-            description="The name of the language model to use for the agent's main interactions. "
-        "Should be in the form: provider/model-name."
+        Literal[
+            "google_genai:gemma-3-12b-it",
+            "google_genai:gemma-3-27b-it",
+            "google_genai:gemini-2.5-flash-lite-preview-06-17",
+        ],
+        {"__template_metadata__": {"kind": "llm"}},
+    ] = Field(
+        default="google_genai:gemma-3-12b-it",
+        description="The name of the language model to use for the agent's main interactions. "
+        "Should be in the form: provider/model-name.",
     )
 
     available_products: list[Literal[*available_prods]] = Field(
-        default = available_prods,
-        description="The list of product's names available for the agent to use."
+        default=available_prods,
+        description="The list of product's names available for the agent to use.",
     )
