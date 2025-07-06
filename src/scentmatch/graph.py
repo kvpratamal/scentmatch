b58eba02341c06447198a843fa76ed1d068dadc4
kvpratama <kvpratama@gmail.com>
2025-07-06 17:50:00 +0900

    Update graph.py

--- src/scentmatch/graph.py
+++ src/scentmatch/graph.py
@@ -4,8 +4,15 @@ from scentmatch.nodes import sales_node
 from scentmatch.configuration import Configuration
 
 # Build the graph
-builder = StateGraph(WorkflowState, input=WorkflowStateInput, output=WorkflowStateOutput, config_schema=Configuration)
+builder = StateGraph(
+    WorkflowState,
+    input=WorkflowStateInput,
+    output=WorkflowStateOutput,
+    config_schema=Configuration,
+)
+
 builder.add_node("sales_node", sales_node)
+
 builder.add_edge(START, "sales_node")
 builder.add_edge("sales_node", END)
 
