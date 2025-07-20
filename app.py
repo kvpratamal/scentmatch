cbc75244f119b0ad1e62895d2a84715ce8159737
kvpratama <kvpratama@gmail.com>
2025-07-20 15:14:21 +0900

    add sidebar

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Scent Match", page_icon=":material/fragrance:")
 
 page_home = st.Page("./pages/page_home.py", title="Home", icon=":material/home:")
+page_lucky = st.Page("./pages/page_lucky.py", title="Feeling Lucky", icon=":material/fragrance:")
 
-pg = st.navigation([page_home])
+pg = st.navigation([page_home, page_lucky])
 
 pg.run()
