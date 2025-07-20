80d7751aee169d69d355e5271399e811fdec6473
kvpratama <kvpratama@gmail.com>
2025-07-20 11:45:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Legendary Scent Match", page_icon=":material/fragrance:")
+st.set_page_config(page_title="Scent Match", page_icon=":material/fragrance:")
 
 page_home = st.Page("./pages/page_home.py", title="Home", icon=":material/home:")
 
